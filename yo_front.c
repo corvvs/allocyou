bd402e55dce02409a5856d35d2aa7f4a2efeca19
河田鷹介 <benefium@gmail.com>
2023-02-24 02:28:28 +0900

    フォルダ移動

--- yo_front.c
+++ yo_front.c
@@ -1,9 +1,19 @@
 #include "yo_internal.h"
 
+#ifdef USE_LIBC
+# define actual_malloc malloc
+# define actual_free free
+# define actual_realloc realloc
+#else
+# define actual_malloc yo_malloc_actual
+# define actual_free yo_free_actual
+# define actual_realloc yo_realloc_actual
+#endif
+
 void*	yo_malloc(size_t n) {
 	DEBUGOUT("** bytes: %zu **", n);
 	SPRINT_START;
-	void	*mem = yo_malloc_actual(n);
+	void	*mem = actual_malloc(n);
 	SPRINT_END;
 	DEBUGSTR("** malloc end **");
 	check_consistency();
@@ -13,7 +23,7 @@ void*	yo_malloc(size_t n) {
 void	yo_free(void *addr) {
 	DEBUGOUT("** addr: %p **", addr);
 	SPRINT_START;
-	yo_free_actual(addr);
+	actual_free(addr);
 	SPRINT_END;
 	DEBUGSTR("** free end **");
 	check_consistency();
@@ -22,7 +32,7 @@ void	yo_free(void *addr) {
 void*	yo_realloc(void *addr, size_t n) {
 	DEBUGOUT("** addr: %p, bytes: %zu **", addr, n);
 	SPRINT_START;
-	void	*mem = yo_realloc_actual(addr, n);
+	void	*mem = actual_realloc(addr, n);
 	SPRINT_END;
 	DEBUGSTR("** realloc end **");
 	check_consistency();
