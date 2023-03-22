07df32478eab8507e85eb05d98a2a9fc87173820
河田鷹介 <benefium@gmail.com>
2023-03-22 19:45:08 +0900

    show_alloc_mem_ex でだけダンプする

--- srcs/visualize.c
+++ srcs/visualize.c
@@ -25,14 +25,16 @@ static void	dump_chunk_body(const t_yoyo_chunk* chunk) {
 }
 
 // チャンク情報を表示する
-static void	visualize_chunk(const t_yoyo_chunk* chunk) {
+static void	visualize_chunk(const t_yoyo_chunk* chunk, bool exec_hexdump) {
 	yoyo_dprintf(STDOUT_FILENO, "\t\tchunk @ %p: %zu blocks (%zu B)\n",
 		chunk, chunk->blocks, chunk->blocks * BLOCK_UNIT_SIZE);
-	dump_chunk_body(chunk);
+	if (exec_hexdump) {
+		dump_chunk_body(chunk);
+	}
 }
 
 // 指定した zone を可視化する
-static void	visualize_locked_zone(t_yoyo_zone* zone) {
+static void	visualize_locked_zone(t_yoyo_zone* zone, bool exec_hexdump) {
 	// [zone 情報を表示]
 	yoyo_dprintf(STDOUT_FILENO, "\t\tzone %p:", zone);
 	if (zone->blocks_used > 0) {
@@ -46,7 +48,7 @@ static void	visualize_locked_zone(t_yoyo_zone* zone) {
 		t_yoyo_chunk*	chunk = get_chunk_by_index(zone, block_index);
 		if (chunk == NULL) { break; }
 		if (is_used(zone, block_index)) {
-			visualize_chunk(chunk);
+			visualize_chunk(chunk, exec_hexdump);
 		}
 		assert(2 <= chunk->blocks);
 		block_index += chunk->blocks;
@@ -54,7 +56,12 @@ static void	visualize_locked_zone(t_yoyo_zone* zone) {
 }
 
 // TINY / SMALL サブアリーナを, 配下の zone を順次ロックしながら可視化していく.
-static void	visualize_locked_tiny_small_subarena(const char* zone_name, t_yoyo_arena* arena, t_yoyo_zone_type zone_type) {
+static void	visualize_locked_tiny_small_subarena(
+	const char* zone_name,
+	t_yoyo_arena* arena,
+	t_yoyo_zone_type zone_type,
+	bool exec_hexdump
+) {
 	assert(zone_type == YOYO_ZONE_TINY || zone_type == YOYO_ZONE_SMALL);
 	t_yoyo_normal_arena* subarena = zone_type == YOYO_ZONE_TINY ? &arena->tiny : &arena->small;
 	if (subarena->head == NULL) {
@@ -70,7 +77,7 @@ static void	visualize_locked_tiny_small_subarena(const char* zone_name, t_yoyo_a
 		if (!lock_zone(zone)) {
 			return; // 致命的
 		}
-		visualize_locked_zone(zone);
+		visualize_locked_zone(zone, exec_hexdump);
 		t_yoyo_zone*	next = zone->next;
 		if (!unlock_zone(zone)) {
 			return; // 致命的
@@ -82,16 +89,24 @@ static void	visualize_locked_tiny_small_subarena(const char* zone_name, t_yoyo_a
 		n_zone, n_zone > 1 ? "zones" : "zone");
 }
 
-static void	visualize_large_chunk(const t_yoyo_large_chunk* large_chunk) {
+static void	visualize_large_chunk(const t_yoyo_large_chunk* large_chunk, bool exec_hexdump) {
 	const t_yoyo_chunk* chunk = (void*)large_chunk + CEILED_LARGE_CHUNK_SIZE;
 	yoyo_dprintf(STDOUT_FILENO, 
 		"\tlarge chunk @ %p: %zu B (usable: %zu B)\n",
 		large_chunk, large_chunk->memory_byte, chunk->blocks * BLOCK_UNIT_SIZE - CEILED_CHUNK_SIZE
 	);
+	if (exec_hexdump) {
+		dump_chunk_body(chunk);
+	}
 }
 
 // LARGE サブアリーナを可視化する
-static void	visualize_locked_large_subarena(const char* zone_name, t_yoyo_arena* arena, t_yoyo_zone_type zone_type) {
+static void	visualize_locked_large_subarena(
+	const char* zone_name,
+	t_yoyo_arena* arena,
+	t_yoyo_zone_type zone_type,
+	bool exec_hexdump
+) {
 	(void)zone_type;
 	assert(zone_type == YOYO_ZONE_LARGE);
 	t_yoyo_large_arena* subarena = &arena->large;
@@ -102,7 +117,7 @@ static void	visualize_locked_large_subarena(const char* zone_name, t_yoyo_arena*
 	t_yoyo_large_chunk*	large_chunk = subarena->allocated;
 	size_t	n_large_chunk = 0;
 	while (large_chunk != NULL) {
-		visualize_large_chunk(large_chunk);
+		visualize_large_chunk(large_chunk, exec_hexdump);
 		large_chunk = large_chunk->large_next;
 		n_large_chunk += 1;
 	}
@@ -112,24 +127,26 @@ static void	visualize_locked_large_subarena(const char* zone_name, t_yoyo_arena*
 // 指定した subarena を可視化する
 static void	visualize_subarena(
 	const char* zone_name,
-	t_yoyo_arena* arena, t_yoyo_zone_type zone_type,
-	void (visualizer)(const char* zone_name, t_yoyo_arena*, t_yoyo_zone_type)
+	t_yoyo_arena* arena,
+	t_yoyo_zone_type zone_type,
+	void (visualizer)(const char* zone_name, t_yoyo_arena*, t_yoyo_zone_type, bool),
+	bool exec_hexdump
 ) {
 	if (!lock_arena(arena, zone_type)) {
 		return;
 	}
-	visualizer(zone_name, arena, zone_type);
+	visualizer(zone_name, arena, zone_type, exec_hexdump);
 	if (!unlock_arena(arena, zone_type)) {
 		return;
 	}
 }
 
 // 指定した arena を可視化する
-static void	visualize_arena(t_yoyo_arena* arena) {
+static void	visualize_arena(t_yoyo_arena* arena, bool exec_hexdump) {
 	yoyo_dprintf(STDOUT_FILENO, "[arena #%u (%s)]\n", arena->index, arena->multi_thread ? "multi-thread mode" : "single-thread mode");
-	visualize_subarena("TINY", arena, YOYO_ZONE_TINY, visualize_locked_tiny_small_subarena);
-	visualize_subarena("SMALL", arena, YOYO_ZONE_SMALL, visualize_locked_tiny_small_subarena);
-	visualize_subarena("LARGE", arena, YOYO_ZONE_LARGE, visualize_locked_large_subarena);
+	visualize_subarena("TINY", arena, YOYO_ZONE_TINY, visualize_locked_tiny_small_subarena, exec_hexdump);
+	visualize_subarena("SMALL", arena, YOYO_ZONE_SMALL, visualize_locked_tiny_small_subarena, exec_hexdump);
+	visualize_subarena("LARGE", arena, YOYO_ZONE_LARGE, visualize_locked_large_subarena, exec_hexdump);
 }
 
 void	actual_show_alloc_mem(void) {
@@ -138,7 +155,7 @@ void	actual_show_alloc_mem(void) {
 		return;
 	}
 	for (unsigned int i = 0; i < g_yoyo_realm.arena_count; ++i) {
-		visualize_arena(&g_yoyo_realm.arenas[i]);
+		visualize_arena(&g_yoyo_realm.arenas[i], false);
 	}
 }
 
@@ -150,4 +167,7 @@ void	actual_show_alloc_mem_ex(void) {
 	// [履歴の表示]
 	show_history();
 	// [ダンプ付きメモリステータスの表示]
+	for (unsigned int i = 0; i < g_yoyo_realm.arena_count; ++i) {
+		visualize_arena(&g_yoyo_realm.arenas[i], true);
+	}
 }
